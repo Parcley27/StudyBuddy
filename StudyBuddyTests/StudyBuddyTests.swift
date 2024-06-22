f1d9425ffc8f6973865b6d058b46dbfeb6fa6b20
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-06-21 18:14:20 -0700

    Initial Commit

---
+++ StudyBuddyTests/StudyBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBuddyTests.swift
+//  StudyBuddyTests
+//
+//  Created by Pierce Oxley on 2024-06-21.
+//
+
+import Testing
+@testable import StudyBuddy
+
+struct StudyBuddyTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
