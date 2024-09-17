b06725b400179289bf9b2c89036c2b545cf5fb77
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-09-17 01:03:20 -0700

    fix: custom font names

--- StudyBuddy/Views/Profile View/ProfileView.swift
+++ StudyBuddy/Views/Profile View/ProfileView.swift
@@ -110,9 +110,8 @@ struct ProfileView: View {
                             .padding()
                             
                             VStack(alignment: .leading) {
-                                Text("Pierce Oxley")
-                                    .font(.custom("Inter", size: 24))
-                                    //.font(.system(size: 24))
+                                Text("User Name")
+                                    .font(.custom("Inter24pt-Bold", size: 24))
                                 Text("@username")
                                 
                                 HStack(alignment: .top) {
@@ -129,6 +128,18 @@ struct ProfileView: View {
                         
                         Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque, mauris sit amet lacinia semper, sem libero tempus metus, quis pellentesque nulla quam id sem. Nam bibendum est a imperdiet fermentum. Integer sit amet risus risus. Nullam interdum pulvinar tellus, ac dictum arcu semper id. Quisque ornare eget nisi at pharetra. Nunc a eros eu augue accumsan sodales. Quisque vehicula nisi quis turpis cursus, et interdum dolor pulvinar. Duis ultricies rutrum nunc, ac consectetur justo venenatis id. Duis fringilla est lectus, nec mollis nunc consequat eu. Suspendisse ac justo venenatis, commodo ipsum in, cursus nunc. Quisque dictum nisl in nisl auctor, id feugiat turpis congue. Aliquam erat volutpat. Integer tincidunt hendrerit augue, quis feugiat felis porttitor vel. Proin non fringilla mauris, in vehicula massa. Nam est justo, viverra quis risus sed, placerat molestie urna. Etiam volutpat condimentum libero vel hendrerit.")
                             .padding()
+                            .onAppear() {
+                                // Display all fonts available to the app
+                                
+                                for family: String in UIFont.familyNames {
+                                    print(family)
+                                
+                                    for names: String in UIFont.fontNames(forFamilyName: family) {
+                                        print("== \(names)")
+                                        
+                                    }
+                                }
+                            }
                         
                     }
                 }
