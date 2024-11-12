76bdf8a779446b45dea004a6bda290393f66ea4b
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-11-11 22:46:18 -0800

    feat: git test

--- StudyBuddy/Views/Profile View/ProfileView.swift
+++ StudyBuddy/Views/Profile View/ProfileView.swift
@@ -182,6 +182,8 @@ struct ProfileView: View {
                             Spacer()
                         }
                         
+                        Text("Charts here!!")
+                        
                         
                         Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque, mauris sit amet lacinia semper, sem libero tempus metus, quis pellentesque nulla quam id sem. Nam bibendum est a imperdiet fermentum. Integer sit amet risus risus. Nullam interdum pulvinar tellus, ac dictum arcu semper id. Quisque ornare eget nisi at pharetra. Nunc a eros eu augue accumsan sodales. Quisque vehicula nisi quis turpis cursus, et interdum dolor pulvinar. Duis ultricies rutrum nunc, ac consectetur justo venenatis id. Duis fringilla est lectus, nec mollis nunc consequat eu. Suspendisse ac justo venenatis, commodo ipsum in, cursus nunc. Quisque dictum nisl in nisl auctor, id feugiat turpis congue. Aliquam erat volutpat. Integer tincidunt hendrerit augue, quis feugiat felis porttitor vel. Proin non fringilla mauris, in vehicula massa. Nam est justo, viverra quis risus sed, placerat molestie urna. Etiam volutpat condimentum libero vel hendrerit.")
                             .padding()
