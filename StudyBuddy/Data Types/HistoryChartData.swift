ae1af1cca28bfd2b02953216764578fc190f6328
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-11-12 01:47:48 -0800

    feat: add HistoryChartData type

--- StudyBuddy/Data Types/HistoryChartData.swift
+++ StudyBuddy/Data Types/HistoryChartData.swift
@@ -1,5 +1,5 @@
 //
-//  ChartHistoryData.swift
+//  HistoryChartData.swift
 //  StudyBuddy
 //
 //  Created by Pierce Oxley on 11/11/24.
@@ -7,15 +7,23 @@
 
 import Foundation
 
-struct ChartHistoryData: Identifiable {
+struct HistoryChartData: Identifiable {
     let date: Date
     
     let minutesStudied: Int
-    var hoursStudied: Double {
-        return Double(minutesStudied) / 60.0
-        
-    }
     
     let id = UUID()
+    
+    static func mockData(_ length: Int) -> [HistoryChartData] {
+        var mockData: [HistoryChartData] = []
+        
+        for i in 0 ... length - 1 {
+            mockData.append(HistoryChartData(date: Date().minusDays(i) ?? Date(), minutesStudied: Int.random(in: 0 ... 300)))
+            
+        }
+        
+        return mockData
+            
+    }
     
 }
