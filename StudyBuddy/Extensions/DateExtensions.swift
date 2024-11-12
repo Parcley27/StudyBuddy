fdaa179d0220396c1faff89b62e831696c597784
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-11-12 01:47:26 -0800

    feat: extension to subtract days from a Date

--- StudyBuddy/Extensions/DateExtensions.swift
+++ StudyBuddy/Extensions/DateExtensions.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+extension Date {
+    func minusDays(_ days: Int) -> Date? {
+        return Calendar.current.date(byAdding: .day, value: -days, to: self)
+        
+    }
+}
+
+// Usage:
+//let today = Date()
+//if let newDate = today.minusDays(5) {
+//    print("5 days ago: \(newDate)")
+//
+//}
