21ac2851976becf0ada1487bb53323fc96d6ccfd
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-11-12 21:48:00 -0800

    perf: disable font family printouts

--- StudyBuddy/Views/Profile View/ProfileView.swift
+++ StudyBuddy/Views/Profile View/ProfileView.swift
@@ -345,6 +345,7 @@ struct ProfileView: View {
                         
                         Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque, mauris sit amet lacinia semper, sem libero tempus metus, quis pellentesque nulla quam id sem. Nam bibendum est a imperdiet fermentum. Integer sit amet risus risus. Nullam interdum pulvinar tellus, ac dictum arcu semper id. Quisque ornare eget nisi at pharetra. Nunc a eros eu augue accumsan sodales. Quisque vehicula nisi quis turpis cursus, et interdum dolor pulvinar. Duis ultricies rutrum nunc, ac consectetur justo venenatis id. Duis fringilla est lectus, nec mollis nunc consequat eu. Suspendisse ac justo venenatis, commodo ipsum in, cursus nunc. Quisque dictum nisl in nisl auctor, id feugiat turpis congue. Aliquam erat volutpat. Integer tincidunt hendrerit augue, quis feugiat felis porttitor vel. Proin non fringilla mauris, in vehicula massa. Nam est justo, viverra quis risus sed, placerat molestie urna. Etiam volutpat condimentum libero vel hendrerit.")
                             .padding()
+                        /*
                             .onAppear() {
                                 // Display all fonts available to the app
                                 
@@ -352,11 +353,12 @@ struct ProfileView: View {
                                     print(family)
                                 
                                     for names: String in UIFont.fontNames(forFamilyName: family) {
-                                        print("== \(names)")
+                                        print("-- \(names)")
                                         
                                     }
                                 }
                             }
+                         */
                         
                     }
                 }
